67204a4fa95829ef4fd31f2621d8e38007c8a990
HirokawaMasayuki <eip012345678910@gmail.com>
2023-07-06 18:23:24 +0900

    delete no using files

--- src230525/Controller/memoyou.php
+++
@@ -1,5 +0,0 @@
-'bik_1' => $data['bik_1'], 'bik_2' => $data['bik_2'], 'bik_3' => $data['bik_3'], 'bik_4' => $data['bik_4'],
-'bik_5' => $data['bik_5'], 'bik_6' => $data['bik_6'], 'bik_7' => $data['bik_7'], 'bik_8' => $data['bik_8'],
-'bik_9' => $data['bik_9'], 'bik_10' => $data['bik_10'], 'bik_11' => $data['bik_11'], 'bik_12' => $data['bik_12'],
-'bik_13' => $data['bik_13'], 'bik_14' => $data['bik_14'], 'bik_15' => $data['bik_15'], 'bik_16' => $data['bik_16'],
-'bik_17' => $data['bik_17'], 'bik_18' => $data['bik_18'], 'bik_19' => $data['bik_19'], 'bik_20' => $data['bik_20'],
